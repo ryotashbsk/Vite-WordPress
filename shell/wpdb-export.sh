8698a2c5d366411cb3e57a88d79c352609051b56
Ryota Shibasaki <46849828+ryotashbsk@users.noreply.github.com>
2023-11-08 22:34:51 +0900

    Update shells

--- shell/wpdb-export.sh
+++ shell/wpdb-export.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-docker exec -i vite-wp-db sh -c 'mysqldump --default-character-set=binary localdb -u root -proot 2> /dev/null' > ./docker/mysql/initdb.sql
+
+folder_path="./docker/mysql"
+container_name="vite-wp-db"
+docker_command="docker exec -i $container_name sh -c"
+dump_command="mysqldump --default-character-set=binary localdb -u root -proot"
+output_file="./docker/mysql/initdb.sql"
+
+if [ ! -d "$folder_path" ]; then
+    mkdir -p "$folder_path"
+fi
+
+if [ -d "$folder_path" ]; then
+    if $docker_command "$dump_command 2> /dev/null" > "$output_file"; then
+        echo "Database export succeeded"
+    else
+        error_message=$($docker_command "$dump_command 2>&1")
+        echo "Database export failed. Error message: $error_message"
+    fi
+else
+    echo "$folder_path does not exist"
+fi
--- shell/wpdb-import.sh
+++ shell/wpdb-import.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker exec -i vite-wp-db sh -c 'mysql -u root -proot localdb' < ./docker/mysql/initdb.sql
+sql_file="./docker/mysql/initdb.sql"
+container_name="vite-wp-db"
+
+if [ -f "$sql_file" ]; then
+    docker exec -i $container_name sh -c 'mysql -u root -proot localdb' < "$sql_file"
+    echo "Executed SQL file"
+else
+    echo "$sql_file does not exist"
+fi
