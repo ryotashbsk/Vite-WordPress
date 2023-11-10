efe6e51b272a5050c6677e53fb7e5cbfbe3e2f17
Ryota Shibasaki <46849828+ryotashbsk@users.noreply.github.com>
2023-11-10 13:54:49 +0900

    Change the database image from MySQL to MariaDB

--- shell/wpdb-import.sh
+++ shell/wpdb-import.sh
@@ -3,7 +3,7 @@
 sql_file="./docker/mysql/initdb.sql"
 container_name="vite-wp-db"
 docker_command="docker exec -i $container_name sh -c"
-import_command="mysql -u root -proot localdb"
+import_command="mariadb -u root -proot localdb"
 
 if [ -f "$sql_file" ]; then
     if $docker_command "$import_command" < "$sql_file"; then
