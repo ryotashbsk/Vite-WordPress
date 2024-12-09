792a92256c7afa24ddbfe347160eb94d75245d64
Ryota Shibasaki <46849828+ryotashbsk@users.noreply.github.com>
2024-12-09 11:20:40 +0900

    clean functions

--- dist/wp-content/themes/my-theme/functions.php
+++ dist/wp-content/themes/my-theme/functions.php
@@ -1,9 +1,15 @@
 <?php
-locate_template("constants.php", true);
-locate_template("functions/init.php", true);
-locate_template("functions/head.php", true);
-locate_template("functions/admin.php", true);
-locate_template("functions/media.php", true);
-locate_template("functions/utils.php", true);
-locate_template("functions/custom-post-type.php", true);
-locate_template("functions/rest-api.php", true);
+$files = [
+    'constants.php',
+    'functions/init.php',
+    'functions/admin.php',
+    'functions/head.php',
+    'functions/media.php',
+    'functions/utils.php',
+    'functions/custom-post-type.php',
+    'functions/rest-api.php',
+];
+
+foreach ($files as $file) {
+    locate_template($file, true, true);
+}
